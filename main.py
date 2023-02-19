27581d2583ffa054d1c0274bb622b90bd6c5c974
Flavio Mendes <flaviojmendes@gmail.com>
2023-02-19 11:22:45 +0000

    adding python-dotenv

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
                     host="0.0.0.0",
                     port=8000,
                     reload=True,
-                    ssl_keyfile="privkey.pem",
+                    ssl_keyfile="fullchain.pem",
                     ssl_certfile="cert.pem"
                     )
     else:
